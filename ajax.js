81d5dedf2487f50daabee4bf2c4d1d753479823c
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-02-16 15:59:27 -0500

    completed part 4

--- ajax.js
+++ ajax.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   /* Your code goes here */
+  //this is the start of my eventhandler the #ajax_request is the id of the element .on is a method simillar to when
+  //('click', function) is the arguements like the stuff you want to happen
 $('#ajax_request').on('click', function() {
   $.ajax({
     url: 'http://first-ajax-api.herokuapp.com/',
@@ -8,6 +10,17 @@ $('#ajax_request').on('click', function() {
     dataType: 'text'
   });
 //close ajax_request
+});
+
+$('#ajax_ping-pong').on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/pong',
+    method: 'GET',
+    // data: 'html',
+    dataType: 'text'
+  }).fail(function(data) {
+    console.log("didn't work");
+  });
 });
 //the closing brackets below is for the ,document
 });
